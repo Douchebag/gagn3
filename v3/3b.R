2e4185c66f9d4c8325075797d255bda7f744ef21
Douchebag <yesimdingo@gmail.com>
2020-04-25 23:06:44 +0000

    3

--- v3/3b.R
+++ v3/3b.R
@@ -9,7 +9,7 @@ weigh_loss[which.max(weigh_loss$Dec / weigh_loss$Jan), ]
 
 weigh_loss[which.min(weigh_loss$Jan - weigh_loss$Dec), ]
 
-weigh_loss %>% transmute(Individuals, ArangurMarSep = if_else(Feb - Mar > 0 & Aug - Sep > 0, "Gekk vel", "Gekk ekki vel"))
+weigh_loss %>% transmute(Individuals, ArangurMarSep = if_else(Feb - Mar > 0 & Aug - Sep > 0, "Gekk vel", "Ekki vel"))
 
 weigh_loss_extended <- weigh_loss %>% mutate(Success_Rate = -(Dec / Jan - 1) * 100)
 
