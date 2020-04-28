1ee6fcbec1b232f1801169a1ac22671d405303fe
Douchebag <yesimdingo@gmail.com>
2020-04-28 20:03:04 +0000

    Create 4a.R

---
+++ v4/4a.R
@@ -0,0 +1,8 @@
+nofn <- c('Gillian Keene', 'Guðmundur Jónsson', 'Marla Dröfn Diego', 'Konráð Guðmundsson', 'Renate Hertzenslust', 'Sigurður Sívertssen')
+audkenni <- c(1001:1006)
+edlisfraedi <- c(9.4, 7.5, 9.5, 5.0, 6.6, 8.8)
+efnafraedi <- c(8.4, 6.9, 5.5, 9.8, 4.0, 8.8)
+staerdfraedi <- c(9.0, 6.7, 8.5, 5.0, 9.0, 9.7)
+utskrift <- as.Date(c('2012-05-25', '2013-12-17', '2014-05-15', '2014-05-15', '2014-12-19', '2015-05-27'))
+
+nem.data <- data.frame(nofn, audkenni, edlisfraedi, efnafraedi, staerdfraedi, utskrift)
